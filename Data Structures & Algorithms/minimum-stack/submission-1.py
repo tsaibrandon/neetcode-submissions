074c3db9a696ce076aff0b591ad2a756638ba598
Brandon Tsai <100009212+tsaibrandon@users.noreply.github.com>
2026-09-20 21:24:28 -0700

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,26 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minimum = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+        if len(self.minimum) != 0:
+            val = min(val, self.minimum[-1])
+        else:
+            val = min(val, val)
+
+        self.minimum.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minimum.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minimum[-1]
+        
